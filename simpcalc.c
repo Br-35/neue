c4e17cfe2775d54a498054cb2eb2bb85827eafb5
Brian <garbramm@gmail.com>
2025-02-08 15:03:22 +0800

    working_ver

---
+++ simpcalc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+    float input1, input2, output;
+    char operator;
+
+    printf("Input a number: ");
+    scanf("%f", input1);
+
+    printf("Choose operator (+ - * or /): ");
+    scanf("%c", operator);
+
+    switch (operator) {
+        case '': 
+    }
+
+    printf("Input 2nd number: ");
+    scanf("%f", input2);
+
+    printf("%f", output)
+}
